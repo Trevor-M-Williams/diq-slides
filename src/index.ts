cf5820a3fa95b521a7a892528a23c7e5d51b77e2
Trevor Williams <tmw7991@gmail.com>
2024-04-10 14:39:32 -0600

    feat: pdf download

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,74 @@
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   main();
 });
 
 function main() {
-  console.log('Webflow loaded');
-
-  let transformValue = 0;
   const slidesWrapper = document.querySelector('.slides-wrapper') as HTMLElement;
   const slides = document.querySelectorAll('.slides-wrapper .slide');
 
-  if (!slidesWrapper) return;
-  if (!slides.length) return;
+  if (!slidesWrapper || !slides.length) return;
 
-  document.addEventListener('keydown', async function (event) {
+  let currentSlideIndex = 0;
+  const numSlides = slides.length;
+
+  document.addEventListener('keydown', (e) => handleKeydown(e));
+
+  async function downloadSlides() {
+    const doc = new jsPDF({
+      orientation: 'landscape',
+    });
+
+    for (let i = 0; i < numSlides; i++) {
+      currentSlideIndex = i;
+      slidesWrapper.style.transform = `translateX(${i * -100}vw)`;
+      updateSlideNumber();
+      await new Promise((resolve) => setTimeout(resolve, 500));
+
+      const slide = slides[i] as HTMLElement;
+
+      await html2canvas(slide).then((canvas) => {
+        const imgData = canvas.toDataURL('image/jpeg', 1.0);
+        if (i > 0) {
+          doc.addPage();
+        }
+        doc.addImage(imgData, 'JPEG', 10, 10, 280, 150);
+      });
+    }
+
+    slidesWrapper.style.transform = `translateX(${currentSlideIndex * -100}vw)`;
+    doc.save('slides.pdf');
+  }
+
+  function handleKeydown(e: KeyboardEvent) {
+    // override tabs behavior
     setTimeout(() => {
       const activeElement = document.activeElement as HTMLElement;
       if (activeElement) activeElement.blur();
     }, 0);
 
-    const slidesCount = slides.length;
-    const maxTransform = -(slidesCount - 1) * 100;
-
-    if (event.key === 'ArrowRight' && transformValue > maxTransform) {
-      event.preventDefault();
-      transformValue -= 100;
-    } else if (event.key === 'ArrowLeft' && transformValue < 0) {
-      event.preventDefault();
-      transformValue += 100;
+    if (e.key === 'ArrowRight' && currentSlideIndex < numSlides - 1) {
+      e.preventDefault();
+      currentSlideIndex++;
+    } else if (e.key === 'ArrowLeft' && currentSlideIndex > 0) {
+      e.preventDefault();
+      currentSlideIndex--;
+    } else if (e.key === 'd') {
+      e.preventDefault();
+      downloadSlides();
     }
 
-    slidesWrapper.style.transform = `translateX(${transformValue}vw)`;
-  });
+    slidesWrapper.style.transform = `translateX(${currentSlideIndex * -100}vw)`;
+    updateSlideNumber();
+  }
+
+  function updateSlideNumber() {
+    const slideNumberElement = document.querySelector('[data-DIQ="slide-number"]') as HTMLElement;
+    if (slideNumberElement) {
+      slideNumberElement.textContent = (currentSlideIndex + 1).toString();
+    }
+  }
 }
